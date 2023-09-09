75bd39e9136e3727894dc95ff18ce4f1e7757022
vyaches <vyaches.fl@mail.ru>
2023-09-09 12:37:13 +0600

    ready routes

--- tests/add_point.py
+++ tests/add_point.py
@@ -1,15 +1,19 @@
 import requests
-from time import time
+from time import time, sleep
+import json
 
 ip_address = '25.5.221.55'
 
-data = {
-    'robotId':'1',
-    'routeId':'fd087117-5a4c-41cb-b367-b4bf7ae55953',
-    'latitude':54.1232,
-    'longitude':46.3123,
-    'timestamp': int(time())
-    }
-response = requests.post(f'http://{ip_address}:8000/api/add_point', json=data)
-print(response.status_code)
-print(response.content)
+(lat_1, lon_1), (lat_2, lon_2) = (37.5831,55.7446),(37.5942,55.7351)
+response = requests.get(f'https://routing.openstreetmap.de/routed-car/route/v1/driving/{lat_1},{lon_1};{lat_2},{lon_2}?geometries=polyline&steps=true')
+routes = json.loads(response.content.decode('utf-8'))['routes'][0]['legs'][0]['steps']
+for point in routes:
+    lat, lon = point['maneuver']['location']
+    data = {
+        'robotId':'1',
+        'latitude':lat,
+        'longitude':lon,
+        'timestamp': int(time())
+        }
+    response = requests.post(f'http://{ip_address}:8000/api/add_point', json=data)
+    sleep(1)
